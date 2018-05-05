2752d131698c21875c485429ed4e8006c8076d20
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 17:06:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 
+
+
 client.on('message', message => {
            var prefix = ".";
            if (message.content.startsWith(prefix + "id")) {
@@ -30,9 +32,11 @@ client.on('message', message => {
       var id = new  Discord.RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL) 
     .setColor("#707070")
+    .addField(":spy:  الحالة", `**[ ${msg.author.presence.status.toUpperCase()} ]**`, true)
+    .addField(":satellite_orbital:   يلعب", `**[ ${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name} ]**`, true)
     .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
     .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
-    .setFooter(`ProBot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
+    .setFooter(`SpeedBot`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                 
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
 }       });
