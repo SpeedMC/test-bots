def720dc1d40a6c5630f4c58cf06bf3112398d77
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-05 16:59:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client(); 
 const fs = require("fs"); 
-
+const moment = require("moment");
 
 client.on('message', message => {
            var prefix = ".";
