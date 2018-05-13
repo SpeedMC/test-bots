40d4fcbb20832160e4a4377f416cf7f251c37290
SpeedMC <38137719+SpeedMC@users.noreply.github.com>
2018-05-13 21:03:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,28 +8,6 @@ client.on('ready', () => {
 	console.log('I am ready!'); 
   });
 
-client.on("message", (message) => {
-if (message.content.startsWith(prefix+"make")) {
-            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
-        let args = message.content.split(" ").slice(1);
-    message.guild.createChannel('rules', 'text');
-    message.guild.createChannel('welcome', 'text');
-    message.guild.createChannel('chat', 'text');
-    message.guild.createChannel('bo7', 'text');
-    message.guild.createChannel('cuttweet', 'text');
-    message.guild.createChannel('bot-command', 'text');
-    message.guild.createChannel('pic', 'text');
-    message.guild.createChannel('games', 'text');
-    message.guild.createChannel('Swalf', 'voice');
-    message.guild.createChannel('Coffe', 'voice');
-    message.guild.createChannel('Relax', 'voice');
-    message.guild.createChannel('Sweet', 'voice');
-    message.guild.createChannel('Events', 'voice');
-    message.guild.createChannel('Music', 'voice');
-    message.channel.sendMessage('تـم إنـشاء الرومات')
-}
-}); 
-
 client.on('message', message => {                      
 if(!message.channel.guild) return;
    if(message.content.startsWith(prefix + 'colors')) {
